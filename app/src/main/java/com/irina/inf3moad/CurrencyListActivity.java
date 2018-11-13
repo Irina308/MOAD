dafe032ba48b2fafb366c06eb0d382d98c8ae49f
Irina <Irina.ungemach@gmail.com>
2018-11-13 18:08:44 +0100

    * Ex4.2: Show capitol city in maps

--- app/src/main/java/com/irina/inf3moad/CurrencyListActivity.java
+++ app/src/main/java/com/irina/inf3moad/CurrencyListActivity.java
@@ -1,8 +1,12 @@
 package com.irina.inf3moad;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.Arrays;
@@ -10,6 +14,8 @@ import java.util.Arrays;
 public class CurrencyListActivity extends AppCompatActivity {
 
     private ExchangeRateDatabase exchangeRateDatabase = new ExchangeRateDatabase();
+
+    public ExchangeRateAdapter exchangeRateAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,17 @@ public class CurrencyListActivity extends AppCompatActivity {
     private void initList(){
         ListView currencyListView = findViewById(R.id.currencies_lst);
 
-        ExchangeRateAdapter myAdapter = new ExchangeRateAdapter(Arrays.asList(this.exchangeRateDatabase.getExchangeRates()));
-        currencyListView.setAdapter(myAdapter);
+        this.exchangeRateAdapter = new ExchangeRateAdapter(Arrays.asList(this.exchangeRateDatabase.getExchangeRates()));
+        currencyListView.setAdapter(this.exchangeRateAdapter);
+        currencyListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ExchangeRate selectedRate = (ExchangeRate) exchangeRateAdapter.getItem(position);
+                String capitolCity = selectedRate.getCapital();
+                Intent countryIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0`?q=" + capitolCity));
+                startActivity(countryIntent);
+
+            }
+        });
     }
 }
